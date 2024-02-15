0a0151804ea58ba7c28b38289a0794e6fb2e491c
DeepHansda <deephansda921@gmail.com>
2024-02-15 15:00:51 +0000

    some files added

--- src/main.py
+++ src/main.py
@@ -1,18 +1,29 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from fastapi.responses import Response
+from fastapi.requests import Request
 from controllers.MainControllers import MainControllers
 from diffusers import AutoPipelineForText2Image
-import torch
+import torch,io
+from PIL import Image
+
+
 app = FastAPI()
 
 mainControllers = MainControllers()
 
-async def generate_img(prompt):
+class Prompt(BaseModel):
+   prompt:str
+   
+
+async def generate_img(prompt:str) -> Image:
    pipeline = AutoPipelineForText2Image.from_pretrained(
       "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, use_safetensors=True
    ).to("cuda")
 
-   image = await pipeline(prompt, num_inference_steps=25).images[0]
+   image = pipeline(prompt, num_inference_steps=25).images[0]
+   print(type(image))
+
    return image
    
 @app.get('/')
@@ -20,7 +31,8 @@ async def root():
    return await mainControllers.root()
 
 @app.post("/ttimg")
-async def ttimg(request):
-   prompt = request.post.prompt
-   img = await generate_img(prompt)
-   return FileResponse(img,media_type="image/*")
+async def ttimg(prompt:Prompt) -> Response:
+   prompt = prompt.prompt
+   print(prompt)
+   img:bytes = await generate_img(prompt)
+   return Response(content=(io.BytesIO(img)),media_type="image/jpeg")
