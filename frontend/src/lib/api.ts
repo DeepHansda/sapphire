4e7c3a930ccb59175b0bb0695a733f29ee0f8f8d
Deep Hansda <deephansda921@gmail.com>
2024-06-09 08:24:01 +0530

    api url changed in frontend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import { IMG2IMG, TEXT2IMG } from "./const";
 
 
-const api = process.env.NEXT_PUBLIC_API_URL;
+const api = "leading-manually-cowbird.ngrok-free.app";
 const kaggleBaseUrl = 'https://www.kaggle.com/api/v1';
 const get_opt = {
   method: "GET",
